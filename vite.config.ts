7bdba3fcac316bae70a8179b86c28cdc5a7c07e0
minhtv901 <le5875970@gmail.com>
2026-01-06 15:36:33 +0700

    sadasd

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/tech-haven-hub/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
